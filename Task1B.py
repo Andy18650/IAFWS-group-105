e5910f34412a8b217386191aae4ceef273f88eda
Andy18650 <1145662805@qq.com>
2023-02-13 21:18:30 +0000

    Task 1B
    Completed Task 1B

---
+++ Task1B.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2018 Garth N. Wells
+#
+# SPDX-License-Identifier: MIT
+
+from floodsystem.stationdata import build_station_list
+import floodsystem.geo
+
+
+def run():
+    """Requirements for Task 1B"""
+
+    # Build list of stations
+    stations = build_station_list()
+
+    # Use station_by distance to get a list
+    distances=floodsystem.geo.stations_by_distance(stations,(52.2053,0.1218))
+    
+    # Print 10 closest stations
+    print([(station.name,station.town,distance)
+           for (station,distance) in distances[:10]])
+
+    # Print 10 furthest stations
+    print([(station.name,station.town,distance)
+           for (station,distance) in distances[-10:]])
+
+
+if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,11 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine
+
+def stations_by_distance(stations, p):
+    distances=[]
+    for station in stations:
+        distance=haversine(station.coord,p)
+        distances.append((station,distance))
+    return sorted_by_key(distances,1)
