5fdbc06904bd21e1e1907482146ebdaee552084c
Andy18650 <1145662805@qq.com>
2023-02-14 01:40:29 +0000

    One-line geo module
    Completed geo module with only one line of python code implementing function. There is no exception in this library!

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -4,6 +4,8 @@
 """This module contains a collection of functions related to
 geographical data.
 
+There is no exception in this library!
+
 """
 
 from .utils import sorted_by_key  # noqa
@@ -12,8 +14,7 @@ from haversine import haversine
 def stations_by_distance(stations, p):
     """ This function returns a list of (station,distance) tuples
         sorted by distance """
-    return sorted_by_key([(station,haversine(station.coord,p))
-                          for station in stations],1)
+    return sorted_by_key([(station,haversine(station.coord,p)) for station in stations],1)
 
 def stations_within_radius(stations, centre, r):
     """ This function returns a list of all stations within radius r
@@ -33,14 +34,4 @@ def stations_by_river(stations):
 def rivers_by_station_number(stations, N):
     """ This function returns a list of N rivers with the greatest
         number of monitoring stations"""
-    #sortedRivers=sorted_by_key([(river,len(stationList)) for (river,stationList) in list(stations_by_river(stations).items())],1)
-    #[(i,sortedRivers[-i],sortedRivers[-N][1]) for i in range(1,len(sortedRivers)+1)]
-    magicTupleList=zip(range(1,len(stations_by_river(stations))+1),
-                       sorted_by_key([(river,len(stationList)) for (river,stationList) in list(stations_by_river(stations).items())],1)[::-1],
-                       [sorted_by_key([(river,len(stationList)) for (river,stationList) in list(stations_by_river(stations).items())],1)[-N][1]]*len(stations_by_river(stations)))
-
-    return [magicTuple[1] for magicTuple in magicTupleList if magicTuple[0] <= N or magicTuple[1][1] == magicTuple[2]]
-
-    return [sortedRivers[-i] for i in range (1,len(sortedRivers)+1) if i <= N or sortedRivers[-i][1] == sortedRivers[-N][1]]
-    #return [sorted_by_key([(river,len(stationList)) for (river,stationList) in list(stations_by_river(stations).items())],1)[-i] for i in range (1,len(rivers_with_station(stations))+1) if i <= N or sorted_by_key([(river,len(stationList)) for (river,stationList) in list(stations_by_river(stations).items())],1)[-i][1] == sorted_by_key([(river,len(stationList)) for (river,stationList) in list(stations_by_river(stations).items())],1)[-N][1]]
-
+    return [magicTuple[1] for magicTuple in zip(range(1,len(stations_by_river(stations))+1),sorted_by_key([(river,len(stationList)) for (river,stationList) in list(stations_by_river(stations).items())],1)[::-1],[sorted_by_key([(river,len(stationList)) for (river,stationList) in list(stations_by_river(stations).items())],1)[-N][1]]*len(stations_by_river(stations))) if magicTuple[0] <= N or magicTuple[1][1] == magicTuple[2]]
