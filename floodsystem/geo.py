026f22e84edd27b24cb718335de3faff52cb3f6a
Andy18650 <1145662805@qq.com>
2023-02-14 01:18:05 +0000

    Crazy optimizations
    Trying to optimize 1E down to one line (in submodule geo) using some truly crazy methods

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -33,13 +33,9 @@ def stations_by_river(stations):
 def rivers_by_station_number(stations, N):
     """ This function returns a list of N rivers with the greatest
         number of monitoring stations"""
-    rivers={}
-    for station in stations:
-        if station.river in rivers:
-            rivers[station.river]+=1
-        else:
-            rivers[station.river]=1
-    #rivers={river : len(stations_by_river(stations)[river]) for river in rivers_with_station(stations)}
-    sortedRivers=sorted_by_key([(river,rivers[river]) for river in rivers],1)
+    sortedRivers=sorted_by_key([(river,len(stationList)) for (river,stationList) in list(stations_by_river(stations).items())],1)
+    print([(i,sortedRivers[-i],sortedRivers[-N][1]) for i in range(1,len(sortedRivers)+1)]==zip(range(1,len(sortedRivers)+1),sortedRivers[::-1],[sortedRivers[-N][1]]*len(sortedRivers)))
+
     return [sortedRivers[-i] for i in range (1,len(sortedRivers)+1) if i <= N or sortedRivers[-i][1] == sortedRivers[-N][1]]
+    #return [sorted_by_key([(river,len(stationList)) for (river,stationList) in list(stations_by_river(stations).items())],1)[-i] for i in range (1,len(rivers_with_station(stations))+1) if i <= N or sorted_by_key([(river,len(stationList)) for (river,stationList) in list(stations_by_river(stations).items())],1)[-i][1] == sorted_by_key([(river,len(stationList)) for (river,stationList) in list(stations_by_river(stations).items())],1)[-N][1]]
 
